f0e67c29f2b8ff84220bdff738e8b48c7ecea061
Payam <thisispayam@gmail.com>
2017-05-18 17:04:39 -0400

    Setting up the CSS partials,Adding gulp tasks: browser-sync, postcss-import

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    autoprefixer = require('autoprefixer'),
-    postcss = require('gulp-postcss'),
-    cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested');
+var gulp =          require('gulp'),
+    watch =         require('gulp-watch'),
+    postcss =       require('gulp-postcss'),
+    cssvars =       require('postcss-simple-vars'),
+    nested =        require('postcss-nested'),
+    cssImport =     require('postcss-import'),
+    browserSync =   require('browser-sync'),
+    autoprefixer =  require('autoprefixer');
 
 
 //creating a default task
@@ -23,7 +25,7 @@ gulp.task('html', function(){
 // a task to do somthing inside our CSS file (style.css)
 gulp.task('styles',function(){
      return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssvars, nested, autoprefixer])) //all the postcss plugins will go here
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) //all the postcss plugins will go here
     .pipe(gulp.dest('./app/temp/styles')); //where the css file gets compiled
 });
 
@@ -31,12 +33,25 @@ gulp.task('styles',function(){
 
 // creating a watch task using gulp-watch plugin
 gulp.task('watch', function(){
+    browserSync.init({
+        notify:false,
+        server: {
+            baseDir: "app" // where our index.html is
+        }
+    });
     watch('./app/index.html', function(){
-        gulp.start('html'); // watching for any change to our index.html
+        browserSync.reload(); // watching for any change to our index.html and reload on save
     });
     
      watch('./app/assets/styles/**/*.css', function(){ // watch any folder or css file inside assets folder
-        gulp.start('styles'); // watching for any change to our style css file
+        gulp.start('cssInject'); // watching for any change to our style css file
     });
 });
 
+
+//creating a task for CSS injection
+
+gulp.task('cssInject',['styles'], function(){ // It first begin any dependency task (styles),before runing (cssInject). 
+   return gulp.src('./app/temp/styles/style.css')
+   .pipe(browserSync.stream());
+}); 
