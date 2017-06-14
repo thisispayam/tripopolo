fc93f07ff777f900bf2bbb9e812b9a4c6c641015
Payam <thisispayam@gmail.com>
2017-06-14 17:18:58 -0400

    Adding styling and interaction to the manu elements

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 class MobileMenu {
     constructor(){
+        this.siteHeader = $('.site-header')
         this.menuIcon = $('.site-header__menu-icon');
         this.menuContent = $(".site-header__menu-content");
         this.events();
@@ -10,7 +11,8 @@ class MobileMenu {
         this.menuIcon.click(this.toggleTheMenu.bind(this));
     }
     toggleTheMenu(){
-        this.menuContent.toggleClass("site-header__menu-content--is-visible")
+        this.siteHeader.toggleClass("site-header--is-expanded");
+        this.menuContent.toggleClass("site-header__menu-content--is-visible");
     }
 }
 
